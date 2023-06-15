9a2666cf8a0f5d77802d92231e01f7b258fef7f2
Ved Singh Rawat <ved.rawat04@gmail.com>
2023-06-15 20:36:05 +0530

    added color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,35 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        900: '#000000',
+        800: '#0A0A0A',
+        700: '#141414',
+        600: '#1F1F1F',
+        500: '#292929',
+        400: '#333333',
+        300: '#3D3D3D',
+      },
+      secondary: {
+        900: '#BBAEC2',
+        800: '#C4B9CA',
+        700: '#CEC5D3',
+        600: '#D8D1DC',
+        500: '#DFD9E2',
+        400: '#EBE8ED',
+        300: '#F5F4F6',
+      },
+      tertiary: {
+        900: '#00A35C',
+        800: '#00B868',
+        700: '#00CC74',
+        600: '#00E07F',
+        500: '#0FFF95',
+        400: '#33FFA7',
+        300: '#47FFAF',
+      },
+    },
   },
   plugins: [],
 }
